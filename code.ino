4b721efb9aa12d9c3c6cf85775306f56f10efa19
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2022-06-20 18:58:57 +0800

    Update code.ino

--- code.ino
+++ code.ino
@@ -5,11 +5,13 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
+
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 #define MOTOR1 15
 #define MOTOR2 2
+
 char auth[] = BLYNK_AUTH_TOKEN;
 char ssid[] = "Realme 7i Farid";
 char pass[] = "uzumaki suneo";
@@ -27,8 +29,8 @@ int stock1 = 10;
 int stock2 = 10;
 int noty1 = 0;
 int noty2 = 0;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   Blynk.begin(auth, ssid, pass);
   pinMode(interruptPin, INPUT_PULLUP);
@@ -39,20 +41,24 @@ void setup() {
   digitalWrite(MOTOR1, HIGH);
   digitalWrite(MOTOR2, HIGH);
   attachInterrupt(digitalPinToInterrupt(interruptPin), billAcceptor, RISING);
-  //  lcd.begin();
-  //  lcd.backlight();
-  //  lcd.setCursor(0, 0);
-  //  lcd.print(" PLEASE INSERT ");
-  //  lcd.setCursor(0, 1);
-  //  lcd.print("     COIN      ");
+  lcd.begin();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print(" PLEASE INSERT ");
+  lcd.setCursor(0, 1);
+  lcd.print("     COIN      ");
   Timer = millis();
   virtualLCD.print(0, 0, "Stock1 :" + String(stock1) + "   ");
   virtualLCD.print(0, 1, "Stock2 :" + String(stock2) + "   ");
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
   Blynk.run();
+  Serial.println(count);
+  //  if (millis() - Timer > 2000) {
+  //    countreal = count - 100;
+  //    Serial.println("COUNTREAL:" + String(countreal) + " ");
+  //    Timer = millis();
+  //  }
   if (countreal == 1) {
     countreal = 0;
     count = 100;
@@ -64,7 +70,7 @@ void loop() {
     virtualLCD.print(0, 1, "   COIN : " + String(total) + "    ");
 
   }
-  else if (countreal == 2) {
+  if (countreal == 2) {
     countreal = 0;
     count = 100;
     total = total + 0.20;
@@ -75,7 +81,7 @@ void loop() {
     virtualLCD.print(0, 1, "   COIN : " + String(total) + "    ");
 
   }
-  else if (countreal == 3) {
+  if (countreal == 3) {
     countreal = 0;
     count = 100;
     total = total + 0.50;
@@ -86,7 +92,7 @@ void loop() {
     virtualLCD.print(0, 1, "   COIN : " + String(total) + "    ");
 
   }
-  else if (countreal == 4) {
+  if (countreal == 4) {
     countreal = 0;
     count = 100;
     total = total + 0.10;
@@ -97,7 +103,7 @@ void loop() {
     virtualLCD.print(0, 1, "   COIN : " + String(total) + "    ");
 
   }
-  else if (countreal == 5) {
+  if (countreal == 5) {
     countreal = 0;
     count = 100;
     total = total + 0.20;
@@ -108,7 +114,7 @@ void loop() {
     virtualLCD.print(0, 1, "   COIN : " + String(total) + "    ");
 
   }
-  else  if (countreal == 6) {
+  if (countreal == 6) {
     countreal = 0;
     count = 100;
     total = total + 0.50;
@@ -119,74 +125,60 @@ void loop() {
     virtualLCD.print(0, 1, "   COIN : " + String(total) + "    ");
 
   }
-  else {
 
+  if (total >= set1 && digitalRead(5) == LOW && stock1 >= 1) {
+
+    total = 0;
+    stock1--;
+    virtualLCD.print(0, 0, "Stock1 : " + String(stock1) + "  ");
+    virtualLCD.print(0, 1, "   SNACK 1  ");
+
+    //    lcd.setCursor(0, 0);
+    //    lcd.print("    SNACK 1    ");
+    //    lcd.setCursor(0, 1);
+    //    lcd.print("   SELECTED    ");
+    delay(1000);
+    digitalWrite(MOTOR1, LOW);
+    delay(timedelay);
+    digitalWrite(MOTOR1, HIGH);
+    //    lcd.setCursor(0, 0);
+    //    lcd.print(" PLEASE INSERT ");
+    //    lcd.setCursor(0, 1);
+    //    lcd.print("     COIN      ");
+    virtualLCD.clear();
   }
 
+  if (total >= set2 && digitalRead(4) == LOW && stock2 >= 1) {
 
-  if (digitalRead(5) == LOW )
-  {
-    if (total >= set1 && stock1 >= 1)
-    {
+    total = 0;
+    stock2--;
+    virtualLCD.print(0, 1, "Stock2 : " + String(stock2) + "  ");
+    virtualLCD.print(0, 1, "   SNACK 2  ");
 
-      total = 0;
-      stock1--;
-      virtualLCD.print(0, 0, "Stock1 : " + String(stock1) + "  ");
-      virtualLCD.print(0, 1, "   SNACK 1  ");
-      delay(1000);
-      digitalWrite(MOTOR1, LOW);
-      delay(timedelay);
-      digitalWrite(MOTOR1, HIGH);
-      virtualLCD.clear();
+    //    lcd.setCursor(0, 0);
+    //    lcd.print("    SNACK 2    ");
+    //    lcd.setCursor(0, 1);
+    //    lcd.print("   SELECTED    ");
+    delay(1000);
+    digitalWrite(MOTOR2, LOW);
+    delay(timedelay);
+    digitalWrite(MOTOR2, HIGH);
+    //    lcd.setCursor(0, 0);
+    //    lcd.print(" PLEASE INSERT ");
+    //    lcd.setCursor(0, 1);
+    //    lcd.print("     COIN      ");
+    virtualLCD.clear();
 
-    }
-    else {
-
-      virtualLCD.print(0, 1, "   NO STOCK/MONEY  ");
-
-      virtualLCD.clear();
-    }
-  }
-
-
-  if (digitalRead(4) == LOW )
-  {
-    if (total >= set2 && stock2 >= 1)
-    {
-
-      total = 0;
-      stock2--;
-      virtualLCD.print(0, 0, "Stock2 : " + String(stock2) + "  ");
-      virtualLCD.print(0, 1, "   SNACK 2  ");
-      delay(1000);
-      digitalWrite(MOTOR2, LOW);
-      delay(timedelay);
-      digitalWrite(MOTOR2, HIGH);
-      virtualLCD.clear();
-
-    }
-    else {
-
-      virtualLCD.print(0, 1, "   NO STOCK/MONEY  ");
-
-      virtualLCD.clear();
-    }
   }
   if (stock1 == 0 ) {
     Blynk.logEvent("noty", "SNACK 1 OUT OF STOCK");
-    if (millis() - Timer > 43200) {
-      Timer = millis();
-    }
+
   }
   if (stock2 == 0 ) {
     Blynk.logEvent("noty", "SNACK 2 OUT OF STOCK");
-    if (millis() - Timer > 43200) {
-      Timer = millis();
-    }
+
   }
 }
-
-
 void billAcceptor() {
   count++;
   countreal = count - 100;
